0fc3eb941a3bda895f046b06ebc52450a1d0a2e8
David <david@tiqtok.com>
2017-02-17 10:45:10 -0800

    Grouping Sets and Pivoting Data Added

--- Labs_and_Notes.sql
+++ Labs_and_Notes.sql
@@ -757,3 +757,76 @@ SELECT CompanyContact, SUM(SalesAmount) AS Revenue
 FROM CustomerSales
 GROUP BY CompanyContact
 ORDER BY CompanyContact;
+
+
+--Challenge 1: Retrieve Regional Sales Totals
+--Adventure Works sells products to customers in multiple country/regions around the world.
+--1. Retrieve totals for country/region and state/province
+--Tip: Review the documentation for GROUP BY in the Transact-SQL Language Reference.
+--An existing report uses the following query to return total sales revenue grouped by country/region and
+--state/province.
+--SELECT a.CountryRegion, a.StateProvince, SUM(soh.TotalDue) AS Revenue FROM SalesLT.Address AS a
+--JOIN SalesLT.CustomerAddress AS ca ON a.AddressID = ca.AddressID
+--JOIN SalesLT.Customer AS c ON ca.CustomerID = c.CustomerID
+--JOIN SalesLT.SalesOrderHeader as soh ON c.CustomerID = soh.CustomerID GROUP BY a.CountryRegion, a.StateProvince
+--ORDER BY a.CountryRegion, a.StateProvince;
+--You have been asked to modify this query so that the results include a grand total for all sales revenue and a subtotal for each country/region in addition to the state/province subtotals that are already returned.
+
+SELECT a.CountryRegion, a.StateProvince, SUM(soh.TotalDue) AS Revenue
+FROM SalesLT.Address AS a
+JOIN SalesLT.CustomerAddress AS ca
+ON a.AddressID = ca.AddressID
+JOIN SalesLT.Customer AS c
+ON ca.CustomerID = c.CustomerID
+JOIN SalesLT.SalesOrderHeader as soh
+ON c.CustomerID = soh.CustomerID
+GROUP BY a.CountryRegion, a.StateProvince
+ORDER BY a.CountryRegion, a.StateProvince;
+
+
+
+--2. Indicate the grouping level in the results
+--Tip: Review the documentation for the GROUPING_ID function in the Transact-SQL Language Reference.
+--Modify your query to include a column named Level that indicates at which level in the total, country/region, and state/province hierarchy the revenue figure in the row is aggregated. For example, the grand total row should contain the value ‘Total’, the row showing the subtotal for United States should contain the value ‘United States Subtotal’, and the row showing the subtotal for California should contain the value ‘California Subtotal’.
+
+SELECT a.CountryRegion, a.StateProvince, SUM(soh.TotalDue) AS Revenue
+FROM SalesLT.Address AS a
+JOIN SalesLT.CustomerAddress AS ca
+ON a.AddressID = ca.AddressID
+JOIN SalesLT.Customer AS c
+ON ca.CustomerID = c.CustomerID
+JOIN SalesLT.SalesOrderHeader as soh
+ON c.CustomerID = soh.CustomerID
+GROUP BY ROLLUP(a.CountryRegion, a.StateProvince)
+ORDER BY a.CountryRegion, a.StateProvince;
+
+--3. Add a grouping level for cities
+--Extend your query to include a grouping for individual cities.
+
+SELECT a.CountryRegion, a.StateProvince, a.City,
+CHOOSE (1 + GROUPING_ID(a.CountryRegion) + GROUPING_ID(a.StateProvince) + GROUPING_ID(a.City), a.City + ' Subtotal', a.StateProvince + ' Subtotal', a.CountryRegion + ' Subtotal', 'Total') AS Level,
+SUM(soh.TotalDue) AS Revenue
+FROM SalesLT.Address AS a
+JOIN SalesLT.CustomerAddress AS ca
+ON a.AddressID = ca.AddressID
+JOIN SalesLT.Customer AS c
+ON ca.CustomerID = c.CustomerID
+JOIN SalesLT.SalesOrderHeader as soh
+ON c.CustomerID = soh.CustomerID
+GROUP BY ROLLUP(a.CountryRegion, a.StateProvince, a.City)
+ORDER BY a.CountryRegion, a.StateProvince, a.City;
+
+--Challenge 2: Retrieve Customer Sales Revenue by Category
+--Adventure Works products are grouped into categories, which in turn have parent categories (defined in the SalesLT.vGetAllCategories view). Adventure Works customers are retail companies, and they may place orders for products of any category. The revenue for each product in an order is recorded as the LineTotal value in the SalesLT.SalesOrderDetail table.
+--1. Retrieve customer sales revenue for each parent category
+--Retrieve a list of customer company names together with their total revenue for each parent category in Accessories, Bikes, Clothing, and Components.
+
+SELECT * FROM
+(SELECT cat.ParentProductCategoryName, cust.CompanyName, sod.LineTotal
+ FROM SalesLT.SalesOrderDetail AS sod
+ JOIN SalesLT.SalesOrderHeader AS soh ON sod.SalesOrderID = soh.SalesOrderID
+ JOIN SalesLT.Customer AS cust ON soh.CustomerID = cust.CustomerID
+ JOIN SalesLT.Product AS prod ON sod.ProductID = prod.ProductID
+ JOIN SalesLT.vGetAllCategories AS cat ON prod.ProductcategoryID = cat.ProductCategoryID) AS catsales
+PIVOT (SUM(LineTotal) FOR ParentProductCategoryName IN ([Accessories], [Bikes], [Clothing], [Components])) AS pivotedsales
+ORDER BY CompanyName;
