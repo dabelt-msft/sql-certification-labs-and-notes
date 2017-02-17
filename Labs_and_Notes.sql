8c898428a57ec89c9da171cadd0b3f57dd2babc3
David <david@tiqtok.com>
2017-02-17 12:17:52 -0800

    Modifying Data Lab Added

--- Labs_and_Notes.sql
+++ Labs_and_Notes.sql
@@ -830,3 +830,98 @@ SELECT * FROM
  JOIN SalesLT.vGetAllCategories AS cat ON prod.ProductcategoryID = cat.ProductCategoryID) AS catsales
 PIVOT (SUM(LineTotal) FOR ParentProductCategoryName IN ([Accessories], [Bikes], [Clothing], [Components])) AS pivotedsales
 ORDER BY CompanyName;
+
+
+--Section 4 - Module 9
+--Modifying Data
+
+Create Table SalesLT.CallLog
+(
+CallID int IDENTITY PRIMARY KEY NOT NULL,
+CallTime datetime NOT NULL DEFAULT GETDATE(),
+SalesPerson nvarchar(256) NOT NULL,
+CustomerId int NOT NULL REFERENCES SalesLT.Customer(CustomerID),
+PhoneNumber nvarchar(25) NOT NULL, 
+Notes nvarchar(max) NULL
+);
+GO
+
+INSERT INTO SalesLT.CallLog -- "INTO" in "INSERT INTO" is optional
+VALUES
+('2015-01-01T12:30:00', 'adventure-works\pamela0', 1, '562-965-8007', 'Returning call about x y z')
+
+SELECT * FROM SalesLT.CallLog
+
+INSERT SalesLT.CallLog -- "INTO" in "INSERT INTO" is optional
+VALUES
+(DEFAULT, 'adventure-works\pamela0', 1, '562-965-8007', NULL)
+
+--Challenge 1: Inserting Products
+--Each Adventure Works product is stored in the SalesLT.Product table, and each product has a unique ProductID identifier, which is implemented as an IDENTITY column in the SalesLT.Product table. Products are organized into categories, which are defined in the SalesLT.ProductCategory table. The products and product category records are related by a common ProductCategoryID identifier, which is an IDENTITY column in the SalesLT.ProductCategory table.
+--Tip: Review the documentation for INSERT in the Transact-SQL Language Reference.
+--1. Insert a product
+--Adventure Works has started selling the following new product. Insert it into the SalesLT.Product table, using default or NULL values for unspecified columns:
+--After you have inserted the product, run a query to determine the ProductID that was generated. Then run a query to view the row for the product in the SalesLT.Product table.
+
+INSERT INTO SalesLT.Product (Name, ProductNumber, StandardCost, ListPrice, ProductCategoryID, SellStartDate)
+VALUES
+('LED Lights', 'LT-L123', 2.56, 12.99, 37, GETDATE());
+
+
+SELECT SCOPE_IDENTITY();
+
+SELECT * FROM SalesLT.Product
+WHERE ProductID = SCOPE_IDENTITY();
+
+--2. Insert a new category with two products
+--Adventure Works is adding a product category for ‘Bells and Horns’ to its catalog. The parent category for the new category is 4 (Accessories). This new category includes the following two new products:
+--Write a query to insert the new product category, and then insert the two new products with the appropriate ProductCategoryID value.
+--After you have inserted the products, query the SalesLT.Product and SalesLT.ProductCategory tables to verify that the data has been inserted.
+
+INSERT INTO SalesLT.ProductCategory (ParentProductCategoryID, Name)
+VALUES
+(4, 'Bells and Horns');
+
+INSERT INTO SalesLT.Product (Name, ProductNumber, StandardCost, ListPrice, ProductCategoryID, SellStartDate)
+VALUES
+('Bicycle Bell', 'BB-RING', 2.47, 4.99, IDENT_CURRENT('SalesLT.ProductCategory'), GETDATE()),
+('Bicycle Horn', 'BH-PARP', 1.29, 3.75, IDENT_CURRENT('SalesLT.ProductCategory'), GETDATE());
+
+SELECT c.Name As Category, p.Name AS Product
+FROM SalesLT.Product AS p
+JOIN SalesLT.ProductCategory as c ON p.ProductCategoryID = c.ProductCategoryID
+WHERE p.ProductCategoryID = IDENT_CURRENT('SalesLT.ProductCategory');
+
+--Challenge 2: Updating Products
+--You have inserted data for a product, but the pricing details are not correct. You must now update the records you have previously inserted to reflect the correct pricing.
+--Tip: Review the documentation for UPDATE in the Transact-SQL Language Reference.
+--1. Update product prices
+--The sales manager at Adventure Works has mandated a 10% price increase for all products in the Bells and Horns category. Update the rows in the SalesLT.Product table for these products to increase their price by 10%.
+
+UPDATE SalesLT.Product
+SET ListPrice = ListPrice * 1.1
+WHERE ProductCategoryID =
+	(SELECT ProductCategoryID FROM SalesLT.ProductCategory WHERE Name = 'Bells and Horns');
+
+--2. Discontinue products
+--The new LED lights you inserted in the previous challenge are to replace all previous light products. Update the SalesLT.Product table to set the DiscontinuedDate to today’s date for all products in the Lights category (Product Category ID 37) other than the LED Lights product you inserted previously.
+
+UPDATE SalesLT.Product
+SET DiscontinuedDate = GETDATE()
+WHERE ProductCategoryID = 37
+AND ProductNumber <> 'LT-L123';
+
+
+--Challenge 3: Deleting Products
+--The Bells and Horns category has not been successful, and it must be deleted from the database.
+--Tip: Review the documentation for DELETE in the Transact-SQL Language Reference.
+--1. Delete a product category and its products
+--Delete the records foe the Bells and Horns category and its products. You must ensure that you delete the records from the tables in the correct order to avoid a foreign-key constraint violation.
+
+DELETE FROM SalesLT.Product
+WHERE ProductCategoryID =
+	(SELECT ProductCategoryID FROM SalesLT.ProductCategory WHERE Name = 'Bells and Horns');
+
+DELETE FROM SalesLT.ProductCategory
+WHERE ProductCategoryID =
+	(SELECT ProductCategoryID FROM SalesLT.ProductCategory WHERE Name = 'Bells and Horns');
